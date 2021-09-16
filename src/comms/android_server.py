1faf1cfe08a978c423618e92b000222e907b9baa
danielchua2602 <61575401+danielchua2602@users.noreply.github.com>
2021-09-16 18:32:37 +0800

    android bluetooth

--- src/comms/android_server.py
+++ src/comms/android_server.py
@@ -1,19 +1,86 @@
-import bluetooth as bt
+from bluetooth import *
 
 
 class Android_Server:
     def __init__(self):
-        self.a = 1
+        self.server_sock = None
+        self.client_sock = None
+
+        self.server_sock = BluetoothSocket(RFCOMM)
+        self.server_sock.bind(("",PORT_ANY))
+        self.server_sock.listen(1)
+        self.port = self.server_sock.getsockname()[1]
+        self.uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+
+        advertise_service(self.server_sock, "MDPGroup29",
+                          service_id=self.uuid,
+                          service_classes=[self.uuid, SERIAL_PORT_CLASS],
+                          profiles=[SERIAL_PORT_PROFILE]
+                          )
+
+        #if(self.port != 1):
+        #    self.server_sock.close()
+        #    Android_Server().init()
+        #    print("Wrong port")
+
+        print("Waiting for connection on RFCOMM channel %d" % self.port)
 
     def connect(self):
-        self.a = 2
-        print("TODO: Bluetooth connect.")
+        while True:
+            connected = False
+            try:
+                if self.client_sock is None:
+                    self.client_sock, client_info = self.server_sock.accept()
+                    print("Accepted connection from ", client_info)
+                    connected = True
+
+            except Exception as error:
+                print("Connection failed: " + str(error))
+
+            if not connected:
+                break
+
+            print("Attempting to retry connection with Android")
 
     def disconnect(self):
-        return
+        try:
+            # connected to a client socket
+            if self.client_sock is not None:
+                self.client_sock.close()
+                self.client_sock = None
+
+            print("Android disconnected Successfully")
+
+        except Exception as error:
+            print("Android disconnect failed: " + str(error))
+
+    def clear_socket(self):
+        try:
+            if self.client_sock is not None:
+                self.client_sock.close()
+                self.client_sock = None
+
+            if self.server_sock is not None:
+                self.server_sock.close()
+                self.server_sock = None
+
+            print("Cleared both server and client socket")
+
+        except Exception as error:
+            print("Clear socket failed: " + str(error))
+
 
     def read(self):
-        return
+        try:
+            data = self.client_sock.recv(1024).strip()
+            if len(data) == 0: return None
+            print ("From android: [%s]" % data)
+            return data
+
+        except Exception as error:
+            print ("Android read failed: " + str(error))
+            raise error
 
     def write(self, message):
         try:
@@ -24,3 +91,8 @@ class Android_Server:
         except Exception as error:
             print("Android write failed: " + str(error))
             raise error
+
+
+if __name__ == '__main__':
+    Android_Server().connect()
+
