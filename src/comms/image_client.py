1d21a2ddc1093d98a0284020ed2611cfc3318cb8
NicChoo96 <nic_choo_96@hotmail.com>
2021-09-28 00:35:38 +0800

    Updated algo_server

---
+++ src/comms/image_client.py
@@ -0,0 +1,59 @@
+import time
+import grpc
+
+from src.comms import imagecomm_pb2
+from src.comms import imagecomm_pb2_grpc
+
+from picamera import PiCamera
+from picamera.array import PiRGBArray
+
+class Image_Client:
+    
+    def take_picture():
+        try:
+            camera = PiCamera(resolution = '640x480')
+            # 3D RGB numpy array (row, col, colour)
+            picArray = PiRGBArray(camera)
+            
+            # camera warm up time
+            time.sleep(2)
+            # OpenCV takes bgr
+            camera.capture(picArray, format='bgr')
+            image = picArray.array
+            camera.close()
+            
+            print('Image taken successfully')
+
+        except Exception as error:
+            print('Error while taking picture: ' + str(error))
+        
+        # converts the image into a 1D array   
+        array = image.reshape(-1)
+        #print(array)
+        return array
+
+    def process_pic():
+        try:
+            channel = grpc.insecure_channel('192.168.29.202:50051')
+            stub = imagecomm_pb2_grpc.ImageCommStub(channel)
+            
+            # take picture and convert it to 1D
+            picture = take_picture()
+        
+            # update image on server
+            request = imagecomm_pb2.PicArray()
+            request.image.extend(picture)
+            
+            # result of the image recognition model
+            print("Image processing...")
+            result = stub.ProcessImage(request)
+            
+            print(result)
+            
+        except Exception as error:
+            print("Error when processing picture: " + str(error))
+        
+
+if __name__ == "__main__":
+    process_pic()
+
---
+++ src/comms/string_data.py
@@ -0,0 +1,8 @@
+def init():
+    global obs_value
+    global robot_coord
+    s_value = "No value"
+    robot_coord = "No value"
+
+
+        
